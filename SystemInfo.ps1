4608f93a3c8093741a77e66831dc2637ce333434
ellien1975 <ellienhash@gmail.com>
2026-02-15 20:20:32 +0300

    Change user credentials in SystemInfo.ps1
    Updated user credentials for the script.

--- SystemInfo.ps1
+++ SystemInfo.ps1
@@ -1,6 +1,6 @@
 Import-Module ActiveDirectory
-$userName = ''
-$userPassword = ''
+$userName = 'korone'
+$userPassword = 'CHANGEME2023!'
 $psCreds = ConvertTo-SecureString $userPassword -AsPlainText -Force
 $Computers = New-Object -TypeName "System.Collections.ArrayList"
 $Computer = $(Get-ADComputer -Filter * | Select-Object Name)
